e25fa109467e8546cf0dc9cf190e5f0cabd5f3a6
Mehemmed Qurbanov <mahammadag@code.edu.az>
2022-12-23 19:28:51 +0400

    hesim

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace fenerbahca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
